f42dcddf4aaa2990aa512f55f096ea61b59aa155
caiohbe <caiohbe@gmail.com>
2022-11-19 14:30:27 -0300

    post sign-up

--- index.js
+++ index.js
@@ -18,6 +18,52 @@ try {
     console.log(err)
 }
 
+const signUpSchema = joi.object({
+    name: joi.string().alphanum().required(),
+    email: joi.string().required(),
+    password: joi.string().required(),
+    repeatPassword: joi.string().required()
+})
+
+app.post("/sign-up", async (req, res) => {
+    const body = req.body
+
+    const password = bcrypt.hashSync(body.password, 10)
+
+    const user = {
+        ...body,
+        password
+    }
+
+    const validation = signUpSchema.validate(user, { abortEarly: false })
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message)
+        res.status(422).send(errors)
+        return
+    }
+
+    if (body.password !== body.repeatPassword) {
+        res.status(409).send("As senhas precisam ser iguais.")
+        return
+    }
+
+    try {
+        const signUps = await db.collection("sign-ups").find().toArray()
+
+        if (signUps.find(s => s.email === user.email)) {
+            res.status(409).send("Usuário já cadastrado.")
+            return
+        }
+
+        db.collection("sign-ups").insertOne(user)
+        res.status(201)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
 app.listen(5000, () => {
     console.log('Running at port 5000')
 })
